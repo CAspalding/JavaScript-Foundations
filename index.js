2bcf690c0849f46cbb0c43dd83ade6437209a09e
Cassidy Spalding <cassidyspalding@gmailcom>
2020-06-20 16:37:42 -0600

    Completed Task 2

--- index.js
+++ index.js
@@ -35,8 +35,11 @@ Hint #2: you'll need to use the `math` object for parts of this calculation!
 When your math is correct, monthlyRate will equal 1073.64
 */
 
-
-
+var n1 = Math.pow(1 + monthlyInterestRate, periods);
+var numerator = principal * n1 * monthlyInterestRate;
+var denominator = n1 - 1;
+var monthlyRate = numerator/denominator;
+console.log(monthlyRate);
 
 // 🏡 Task 3: Function
 /* Create a function called `mortgageCalculator` that combines all of the steps from task 1 and 2 and returns a sentence "{Name}, your monthly rate is ${monthlyRate}"
